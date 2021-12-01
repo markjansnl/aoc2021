6481ba7478ab9d87d70163978a960e32dedab91e
Mark Jans <mark@markjans.nl>
2021-12-01 09:29:30 +0100

    Aoc01 done

--- aoc01/src/bin/a.rs
+++ aoc01/src/bin/a.rs
@@ -1,10 +1,20 @@
 use aoc01::input;
 
-fn main() {
+pub fn count_larger(input: &str) -> usize {
+    input
+        .lines()
+        .map(|line| line.parse::<u32>().unwrap())
+        .fold((0, u32::MAX), |(count, prev), number| {
+            (if number > prev { count + 1 } else { count }, number)
+        })
+        .0
+}
 
+fn main() {
+    println!("{}", count_larger(input::USER));
 }
 
 #[test]
 fn test_example1() {
-
+    assert_eq!(7, count_larger(input::EXAMPLE));
 }
--- aoc01/src/bin/b.rs
+++ aoc01/src/bin/b.rs
@@ -1,10 +1,23 @@
 use aoc01::input;
 
-fn main() {
+pub fn count_larger(input: &str) -> usize {
+    input
+        .lines()
+        .map(|line| line.parse::<u32>().unwrap())
+        .collect::<Vec<u32>>()
+        .windows(3)
+        .map(|window| window.iter().sum())
+        .fold((0, u32::MAX), |(count, prev), number| {
+            (if number > prev { count + 1 } else { count }, number)
+        })
+        .0
+}
 
+fn main() {
+    println!("{}", count_larger(input::USER));
 }
 
 #[test]
 fn test_example1() {
-    
+    assert_eq!(5, count_larger(input::EXAMPLE));
 }
