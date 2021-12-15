a92144e50da1a7d02815b4de425728c283af5e9b
Mark Jans <mark@markjans.nl>
2021-12-15 13:20:35 +0100

    Added aoc09

---
+++ aoc09/src/bin/a.rs
@@ -0,0 +1,14 @@
+use aoc09::input;
+
+fn sum_risk_levels(input: &str) -> usize {
+    0
+}
+
+fn main() {
+    println!("{}", sum_risk_levels(input::USER));
+}
+
+#[test]
+fn test_example() {
+    assert_eq!(15, sum_risk_levels(input::EXAMPLE));
+}
