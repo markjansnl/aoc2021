294d50a4de9eb889c1b9dde6c60c8634beefad00
Mark Jans <mark@markjans.nl>
2021-12-26 19:17:16 +0100

    Aoc 13 small improvement

--- aoc13/src/bin/b.rs
+++ aoc13/src/bin/b.rs
@@ -4,20 +4,14 @@ fn print_dots(input: &str) {
     let (mut dots, instructions) = parse_input(input);
     dots = instructions.into_iter().fold(dots, fold);
 
-    let (min_x, min_y, max_x, max_y) = dots.iter().fold(
-        (u16::MAX, u16::MAX, 0, 0),
-        |(prev_min_x, prev_min_y, prev_max_x, prev_max_y), &(x, y)| {
-            (
-                prev_min_x.min(x),
-                prev_min_y.min(y),
-                prev_max_x.max(x),
-                prev_max_y.max(y),
-            )
-        },
-    );
+    let (max_x, max_y) = dots
+        .iter()
+        .fold((0, 0), |(prev_max_x, prev_max_y), &(x, y)| {
+            (prev_max_x.max(x), prev_max_y.max(y))
+        });
 
-    for y in min_y..=max_y {
-        for x in min_x..=max_x {
+    for y in 0..=max_y {
+        for x in 0..=max_x {
             print!("{}", if dots.contains(&(x, y)) { "#" } else { " " });
         }
         println!();
